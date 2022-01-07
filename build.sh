fd1f62440dc92e03486f4c033cff96af5df62d1e
Adrian Pedziwiatr <180254@users.noreply.github.com>
2022-01-07 14:20:27 +0100

    Bump version to 2.1.0-beta

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 ARGUMENTS="$*"
 
 if [[ "$ARGUMENTS" == *"--no-cache"* ]]; then
-  docker build --no-cache --pull -t "seo2:2.0.1" .
+  docker build --no-cache --pull -t "seo2:2.1.0-beta" .
 else
-  docker build -t "seo2:2.0.1" .
+  docker build -t "seo2:2.1.0-beta" .
 fi
