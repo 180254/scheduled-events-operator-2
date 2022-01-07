a6f944af2f626e8699f891895ea9103b863c8ef2
Adrian Pedziwiatr <180254@users.noreply.github.com>
2022-01-07 14:22:16 +0100

    Bump version to 3.0.0

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 ARGUMENTS="$*"
 
 if [[ "$ARGUMENTS" == *"--no-cache"* ]]; then
-  docker build --no-cache --pull -t "seo2:3.0.0-beta" .
+  docker build --no-cache --pull -t "seo2:3.0.0" .
 else
-  docker build -t "seo2:3.0.0-beta" .
+  docker build -t "seo2:3.0.0" .
 fi
